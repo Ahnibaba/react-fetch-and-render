4107a695ad2edd6b6b938907084565a88af72b65
Ahnibaba <aniebietedet2025@gmail.com>
2024-03-28 20:13:26 +0100

    No object in a cell(single element)

--- src/Cell.js
+++ src/Cell.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import SingleData from './SingleData'
+//import SingleData from './SingleData'
 
 const Cell = ({ cellData, key}) => {
   return (
-    <td key={key}>
-        {typeof cellData === 'object' ? Object.entries(cellData).map(([key, value]) => {
-            return(
-                <SingleData key={key} SingleData={JSON.stringify(value)} />
-            )
-        }) : cellData}
+   
+
+    <td>
+        {cellData}
     </td>
   )
 }
--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import Cell from './Cell'
+import React from 'react';
+import Cell from './Cell';
 
 const Row = ({ item }) => {
   return (
     <tr>
        {Object.entries(item).map(([key, value]) => {
-            return(
-                <Cell key={key} cellData={value} />
-            )
+            // Check if value is an object
+            if (typeof value === 'object' && value !== null) {
+                // If value is an object, iterate over its keys and render each element
+                return Object.values(value).map((element, index) => {
+                    // Check if the element is also an object
+                    if (typeof element === 'object' && element !== null) {
+                        // If element is an object, iterate over its values and render each one
+                        return Object.values(element).map((nestedElement, nestedIndex) => (
+                            <Cell key={key + index + nestedIndex} cellData={JSON.stringify(nestedElement)} />
+                        ));
+                    } else {
+                        // If element is not an object, render it directly
+                        return <Cell key={key + index} cellData={JSON.stringify(element)} />;
+                    }
+                });
+            } else {
+                // If value is not an object, render it directly
+                return <Cell key={key} cellData={JSON.stringify(value)} />;
+            }
        })}
     </tr>
-  )
-}
+  );
+};
 
-export default Row
+export default Row;
